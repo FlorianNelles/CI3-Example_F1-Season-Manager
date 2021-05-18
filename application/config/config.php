613d822b98f39b5a83938c7ce4a5d8c9bcf294a8
FlorianNelles <floriannelles@gmx.de>
2021-05-18 13:15:40 +0200

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'https://codeigniterv3.app';
-$config['base_url'] = 'http://localhost/live_projekt';
+$config['base_url'] = 'http://localhost/live_version3';
 
 /*
 |--------------------------------------------------------------------------
